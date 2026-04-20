99667fc4ef3dae408880ac7d6259cfa0917cad12
Timur <78265814+capzzet@users.noreply.github.com>
2026-04-20 21:45:21 +0600

    add categories, properties, images migrations

---
+++ database/migrations/2026_04_20_153816_create_properties_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 12, 2);
+            $table->string('address');
+            $table->string('city');
+            $table->integer('rooms')->nullable();
+            $table->decimal('area', 8, 2)->nullable();
+            $table->enum('deal_type', ['sale', 'rent']);
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
